5e226ce7b40ad8641fd025e9ca0856e9889386ac
ekarusx-arch <ekarusx@gmail.com>
2026-05-04 12:28:39 +0900

    fix: remove unused DialogHeader import in StatsModal to pass build

--- components/StatsModal.tsx
+++ components/StatsModal.tsx
@@ -6,7 +6,6 @@ import { useTimeboxerStore } from "@/store/useTimeboxerStore";
 import {
   Dialog,
   DialogContent,
-  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
